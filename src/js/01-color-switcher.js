b58c68e403fe2386e5be6d1f535cd89e85c18d08
SHDMYUA <90021141+SHDMYUA@users.noreply.github.com>
2022-06-10 17:17:12 +0300

    first deploy

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+  main: document.body,
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  }
 
+const COLOR_DELAY = 1000;
+let timeoutId = null;
+
+//add listeners to the buttons abd body
+refs.startBtn.addEventListener('click', getRandomHexColor);
+refs.stopBtn.addEventListener('click', onBtnStopClick);
+
+// function random color
+function getRandomHexColor() {
+timeoutId = setInterval(() => {
+  let color = `#${Math.floor(Math.random() * 16777215).toString(16)}`
+  refs.main.style.background = color;
+  refs.startBtn.disabled = true;
+  },
+  COLOR_DELAY);
+  }
+
+//remove Timeout by 'click' "disable"-button 
+function onBtnStopClick() {
+refs.startBtn.disabled = false;
+clearInterval(timeoutId);
+}
